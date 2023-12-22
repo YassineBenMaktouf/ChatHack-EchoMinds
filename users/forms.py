3ccb86d091b0e5e3109a9d9c554fdd91fc4836fe
lou4y <louay.ghazouani@esprit.tn>
2023-12-22 16:46:03 +0100

    2

--- users/forms.py
+++ users/forms.py
@@ -1,4 +1,3 @@
-
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 
@@ -10,9 +9,12 @@ from django.forms.widgets import PasswordInput, TextInput
 # - Create/Register a user (Model Form)
 
 class CreateUserForm(UserCreationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
 
     class Meta:
-
         model = User
         fields = ['username', 'email', 'password1', 'password2']
 
